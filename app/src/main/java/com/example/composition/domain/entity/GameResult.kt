867075d2ae61d37e26ccbb497faf3380ec6519d4
Semjon Zyubin <lesqt58@gmail.com>
2024-02-18 18:12:06 +0400

    lesson_67

---
+++ app/src/main/java/com/example/composition/domain/entity/GameResult.kt
@@ -0,0 +1,9 @@
+package com.example.composition.domain.entity
+
+data class GameResult(
+    val winner: Boolean,
+    val countOfRightAnswers: Int,
+    val countOfQuestions: Int,
+    val gameSettings: GameSettings
+) {
+}
---
+++ app/src/main/java/com/example/composition/domain/entity/Question.kt
@@ -0,0 +1,8 @@
+package com.example.composition.domain.entity
+
+data class Question(
+    val sum: Int,
+    val visibleNumber: Int,
+    val options: List<Int>
+) {
+}
